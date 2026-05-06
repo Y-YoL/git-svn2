ab2ce308c56712948aa2872bedc9c7cf6deb98f4
Y-YoL <11359025+Y-YoL@users.noreply.github.com>
2026-05-06 14:57:49 +0900

    RootCommandの用意

--- src/git-svn2/Program.cs
+++ src/git-svn2/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+using System.CommandLine;
+
+var root = new RootCommand("git svn2 is a simple conduit for changesets between Subversion and Git.");
+
+if(args is []){
+	args = ["--help"];
+}
+
+var parseResult = root.Parse(args);
+return await parseResult.InvokeAsync();
